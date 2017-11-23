563fc0aaf2baeb812a276f59640066392426c013
segerpeter07 <segerpeter07@gmail.com>
2017-11-22 23:47:37 -0500

    Got it working.

--- Webapp/webapp.py
+++ Webapp/webapp.py
@@ -14,11 +14,15 @@ def home():
 @app.route('/selector', methods=['GET', 'POST'])
 def selector():
     if request.method == 'GET':
-        return render_template('selector.html')
+        return render_template('selector.html', i_select = 'camera', o_select = 'lights')
     if request.method == 'POST':
         inputs = request.form.getlist('inputs')
         outputs = request.form.getlist('outputs')
-        return render_template('selector.html')
+        for i in inputs:
+            print(i)
+        for o in outputs:
+            print(o)
+        return render_template('selector.html', i_select = inputs[0], o_select = outputs[0])
 
 
 if __name__ == "__main__":
