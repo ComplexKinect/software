9ba81fe3cd70cdfda8a89e7b07cc07ebf76fbe2c
Vicky McDermott <vickymmcd@gmail.com>
2017-12-06 11:02:18 -0500

    got the webapp working

--- Webapp/webapp.py
+++ Webapp/webapp.py
@@ -2,11 +2,18 @@ import os
 import sys
 import requests
 import time
+from multiprocessing import Process
 from flask import Flask, redirect, render_template, request, session, abort, url_for
 
 from sound_processing import process_signal
 
 app = Flask('flaskapp')
+global audio_p
+audio_p = None
+global video_p
+video_p = None
+global demo_p
+demo_p = None
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -21,10 +28,29 @@ def selector():
         inputs = request.form.getlist('inputs')
         for i in inputs:
             print(i)
+        global audio_p
+        global video_p
+        global demo_p
         selected = inputs[0]
         if selected == 'audio':
-            process_signal.detect_sound()
+            if audio_p != None and audio_p.is_alive():
+                pass
+            else:
+                video_p = stop_process(video_p)
+                demo_p = stop_process(demo_p)
+                audio_p = Process(target=process_signal.detect_sound)
+                audio_p.start()
+        if selected == 'off':
+            video_p = stop_process(video_p)
+            demo_p = stop_process(demo_p)
+            audio_p = stop_process(audio_p)
         return render_template('selector.html', i_select = inputs[0])
+
+def stop_process(my_process):
+    if my_process != None and my_process.is_alive():
+        my_process.terminate()
+        return None
+    return my_process
 
 
 if __name__ == "__main__":
